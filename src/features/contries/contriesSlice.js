6ee9528706e7f53c03abcecf56845d27c3cf8459
yuusufxb <youssefmouha98@gmail.com>
2026-02-12 23:42:32 +0100

    adding json file for contries data

--- src/features/contries/contriesSlice.js
+++ src/features/contries/contriesSlice.js
@@ -1,7 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { asyncThunkCreator, createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  list: ["morocco"],        
+  list: ["marocooo"],        
   status: 'idle',  
   error: null,
 }
