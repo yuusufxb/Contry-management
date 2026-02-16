e6cf46f03e944377f4641ec4229db260d156ff0d
yuusufxb <youssefmouha98@gmail.com>
2026-02-16 18:16:06 +0000

    styling the navbar

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,24 +1,29 @@
+import { Link, useNavigate } from "react-router-dom";
+import './style/navbar_style.css';
 
-import { Link, useNavigate } from "react-router-dom"
 export function Navbar(){
     const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    const path = e.target.value;
-    if (path) 
-        navigate(path); 
-  };
+    const handleChange = (e) => {
+        const path = e.target.value;
+        if (path) navigate(path);
+    };
 
-  return (
-    <div>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/world-total-population'}>total population </Link>
-        <select onChange={handleChange}>
-            <option value="">Select by →</option>
-            <option value="/countries/by-name">Name</option>
-            <option value="/countries/by-capital">Capital</option>
-            <option value="/countries/by-population">Population</option>
-        </select>
-    </div>
-  );
+    return (
+        <div className="navbar">
+            <div className="navbar-links">
+                <Link to="/">Home</Link>
+                <Link to="/world-total-population">Total Population</Link>
+            </div>
+
+            <div className="navbar-select">
+                <select onChange={handleChange}>
+                    <option value="">Select by →</option>
+                    <option value="/countries/by-name">Name</option>
+                    <option value="/countries/by-capital">Capital</option>
+                    <option value="/countries/by-population">Population</option>
+                </select>
+            </div>
+        </div>
+    );
 }
