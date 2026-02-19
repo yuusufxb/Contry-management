ae110b4c6f3473fe4d6290fefda88b16815991fd
yuusufxb <youssefmouha98@gmail.com>
2026-02-19 23:54:59 +0000

    adding a style to home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,20 +2,20 @@ import { useDispatch, useSelector } from "react-redux"
 import { setcontries } from "../features/contries/contriesSlice";
 import { useEffect } from "react";
 import { Await, Link } from "react-router-dom";
+import "./style/home_style.css"
 
 export function Home () {
     const dispatch = useDispatch();
     const getArray = useSelector((item)=> item.contries.list);
     return(
-        <>
-         {getArray.countries.map((co)=>(
-            <>
-            {/* <Link to={"/"}></> */}
-            <h1>{co.name}</h1>
-            <img src={co.flag} alt="country flag" />
-            <h3>{co.capital}</h3>
-            </>
-         ))}
-        </>
+        <div className="countries-container">
+            {getArray.countries.map((co) => (
+            <Link to={"/countries/" + co.id} key={co.id} className="country-card">
+                <h1>{co.name}</h1>
+                <img src={co.flag} alt="country flag" />
+                <h3>{co.capital}</h3>
+            </Link>
+            ))}
+        </div>
     )
 }
