83540a0932a5fc1b8e165fec4e791ff0cc6f3243
yuusufxb <youssefmouha98@gmail.com>
2026-03-04 23:15:15 +0000

    styling the totalPopulation component

--- src/pages/total_population.jsx
+++ src/pages/total_population.jsx
@@ -1,19 +1,25 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchContriesData } from "../features/contries/contriesSlice";
+import './style/totalPop_style.css'
 
-
-export function TotalPopulation(){
+export function TotalPopulation() {
     // API for countries data 'https://restcountries.com/v4/all?fields=name,population,capital,region,flag,language'    
-    const totalPop = useSelector((state)=>state.contries.totalPopulation);
+    const totalPop = useSelector((state) => state.contries.totalPopulation);
     const dispatch = useDispatch();
-    useEffect(()=>{
-            dispatch(fetchContriesData())
-        },[totalPop,dispatch])
-    return(
-        <>
-            <h1>World total population : </h1>
-            <p>{totalPop}</p>
-        </>
+    useEffect(() => {
+        dispatch(fetchContriesData())
+    }, [totalPop, dispatch])
+    return (
+        <div className="population-container">
+            <div className="population-card">
+                <h1 className="population-title">
+                    🌍 World Total Population
+                </h1>
+                <p className="population-number">
+                    {totalPop.toLocaleString()}
+                </p>
+            </div>
+        </div>
     );
 }
