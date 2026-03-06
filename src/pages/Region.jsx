6e4b991295e63e5a54bbe19e53254c231ac78647
yuusufxb <youssefmouha98@gmail.com>
2026-03-06 18:04:05 +0000

    adding html to select by region component

--- src/pages/Region.jsx
+++ src/pages/Region.jsx
@@ -1,9 +1,22 @@
+import { useSelector } from "react-redux";
 
 
 export function Region(){
+
+    const contries = useSelector((item)=>item.contries.fetchedContries.region) ;
+    const uniqueRegion = [...new Set(contries)]
     return(
         <>
         <h1>Select By Region</h1>
+        <select onChange={<></>}>
+            <option value="">Select Region →</option>
+            <option value="Europe"></option>
+            <option value="Africa"></option>
+            <option value="Asia"></option>
+            <option value="Americas"></option>
+            <option value="Oceania"></option>
+            <option value="Antarctic"></option>
+        </select>
         </>
     );    
 }
