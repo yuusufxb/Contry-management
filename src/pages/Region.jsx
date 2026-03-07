80764011c5afe5f1f4ce31d42eeb679d5891b747
yuusufxb <youssefmouha98@gmail.com>
2026-03-07 22:32:42 +0000

    adding methode onchange whenever the user select a region

--- src/pages/Region.jsx
+++ src/pages/Region.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
 
 export function Region() {
-
+    // bringing the contries data
     const contries = useSelector((item) => item.contries.fetchedContries);
     const regions = contries.map((it) => { return it.region });
+    // methode to select the unique values of an array 
     const uniqueRegions = [...new Set(regions)]
     console.log(uniqueRegions)
+    // handling the region selected by the user 
+    const [regionSelected , setregionSelecter] = useState("");
+    const onChange = (e) =>{
+        const param = e.target.value ;
+        if (param) setregionSelecter(param);
+
+    }
+    console.log(regionSelected);
+    // 
+    const contriesByRegion = contries.find((co)=> co.Region == regionSelected);
+    console.log(contriesByRegion);
     return (
         <>
             <h1>Select By Region</h1>
-            <select>
+            <select onChange={onChange}>
                 <option value="">Select Region →</option>
                 {uniqueRegions.map((region)=>(
                 <div>
